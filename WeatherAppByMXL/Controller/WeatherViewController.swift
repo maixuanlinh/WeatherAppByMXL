8a536c0aed92b5de2d1f16cfe445bbaf723d5390
maixuanlinh <maixuanlinhdkh@gmail.com>
2020-11-24 04:36:07 +0200

    tỉring day. but keep moving. let's commit a small thing.

--- WeatherAppByMXL/Controller/WeatherViewController.swift
+++ WeatherAppByMXL/Controller/WeatherViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class WeatherViewController: UIViewController {
 
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var cityLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
